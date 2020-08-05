7918554d4a9334fdff63372643fed079fdcbeebc
jesus-rs-0803 <jesus.enggel.rivera.santiago@everis.com>
2020-08-04 23:17:54 -0500

    cascaron microservice-credit

---
+++ src/main/java/com/bootcam/MicroserviceCreditApplication.java
@@ -0,0 +1,13 @@
+package com.bootcam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCreditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcam/MicroserviceCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
