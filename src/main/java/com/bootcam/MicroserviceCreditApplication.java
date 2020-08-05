438a75a7e41d1b872e1452ff7af8031497235487
jesus-rs-0803 <jesus.enggel.rivera.santiago@everis.com>
2020-08-05 12:34:38 -0500

    Actualizacion microservice-credit

--- src/main/java/com/bootcam/MicroserviceCreditApplication.java
+++ src/main/java/com/bootcam/MicroserviceCreditApplication.java
@@ -2,12 +2,24 @@ package com.bootcam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceCreditApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceCreditApplication.class, args);
 	}
+	
+	 @RestController
+	   class HelloController {
+	      @RequestMapping("/hello")
+	      public String serviceInstancesByApplicationName() {
+	         return "HelloWorld!";
+	      }
+	   }
 
 }
